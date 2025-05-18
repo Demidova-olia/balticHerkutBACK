168e70b540b8919ede5410d6fbeabb620f980d04
Demidova.olia <demidova.olia@gmail.com>
2025-05-18 16:52:20 +0300

    save

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,33 +5,8 @@ const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 const path = require("path");
 const mongoose = require("mongoose");
-const { uploadToCloudinary } = require("../utils/cloudinary");
+const { uploadToCloudinary } = require("../utils/uploadToCloudinary");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const uploadToCloudinary = (fileBuffer, filename) => {
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      {
-        folder: "products",
-        resource_type: "image",
-        public_id: filename.split(".")[0],
-      },
-      (error, result) => {
-        if (error) return reject(error);
-        resolve({
-          url: result.secure_url,
-          public_id: result.public_id,
-        });
-      }
-    );
-    streamifier.createReadStream(fileBuffer).pipe(stream);
-  });
-};
 
 const createProduct = async (req, res) => {
   try {
