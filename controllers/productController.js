38e42876a0320d861308024016d7426a313958df
Demidova.olia <demidova.olia@gmail.com>
2025-05-18 16:45:40 +0300

    save

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,6 +5,7 @@ const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 const path = require("path");
 const mongoose = require("mongoose");
+const { uploadToCloudinary } = require("../utils/cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
--- utils/uploadToCloudinary.js
+++ utils/uploadToCloudinary.js
@@ -1,17 +1,23 @@
-const cloudinary = require("../config/cloudinary");
-const streamifier = require("streamifier");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const uploadToCloudinary = (buffer, filename) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      {
-        folder: "products",
-        public_id: filename.split(".")[0],
-        resource_type: "image",
-      },
+      { folder: 'products', public_id: filename },
       (error, result) => {
-        if (error) return reject(error);
-        resolve(result.secure_url);
+        if (error) {
+          console.error("Cloudinary error:", error);
+          reject(error);
+        } else {
+          resolve({ url: result.secure_url, public_id: result.public_id });
+        }
       }
     );
 
@@ -19,4 +25,4 @@ const uploadToCloudinary = (buffer, filename) => {
   });
 };
 
-module.exports = uploadToCloudinary;
+module.exports = { uploadToCloudinary };
