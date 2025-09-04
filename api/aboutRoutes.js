8f28c80e247742202088fc22dab956ef059dca04
Demidova.olia <demidova.olia@gmail.com>
2025-09-04 13:49:14 +0300

    save

--- api/aboutRoutes.js
+++ api/aboutRoutes.js
@@ -3,19 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middlewares/authMiddleware");
-const rolesMiddleware = require("../middlewares/rolesMiddleware"); // уже есть у вас
-const upload = require("../middlewares/multer"); // memoryStorage
+const rolesMiddleware = require("../middlewares/rolesMiddleware");
+const upload = require("../middlewares/multer"); 
 
 const {
   getAbout,
   updateAbout,
 } = require("../controllers/aboutController");
 
-// GET /api/about
 router.get("/", getAbout);
 
-// PUT /api/about  (только админ)
-// принимает FormData: payload(JSON) + heroImage / storeImage / requisitesImage (files)
 router.put(
   "/",
   authMiddleware,
